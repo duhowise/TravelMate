169cd0f1817bd9a8966b1bec8960228d442156e0
duho wise <wsduho>
2017-08-06 21:23:14 +0000

    Remove Dependency Entity Framework

--- TravelMate/Src/TravelMate.Web/App_Start/IdentityConfig.cs
+++ TravelMate/Src/TravelMate.Web/App_Start/IdentityConfig.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
--- TravelMate/Src/TravelMate.Web/Models/IdentityModels.cs
+++ TravelMate/Src/TravelMate.Web/Models/IdentityModels.cs
@@ -1,9 +1,7 @@
 ﻿using System.Configuration;
-using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using TravelMate.Data;
 
 namespace TravelMate.Web.Models
